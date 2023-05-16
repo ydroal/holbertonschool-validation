20eb2fc46bc1c34467a60568f2a8a78068d7f2f8
ydroal <droal.yoko@gmail.com>
2023-05-16 12:08:35 +0200

    add path

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
 if ! dpkg -s make >/dev/null 2>&1; then
-  apt-get install -y make
+  sudo apt-get install -y make
 fi
 if ! dpkg -s curl >/dev/null 2>&1; then
-  apt-get install -y curl
+  sudo apt-get install -y curl
 fi
 if ! dpkg -s hugo >/dev/null 2>&1; then
   # Download lastest release of 'Hugo'
   curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
   # Install the latest version of 'Hugo'
-  apt-get install ./install_hugo.deb
+  sudo apt-get install ./install_hugo.deb
 fi
 # Install golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.42.1
+
+export PATH=$PATH:$(go env GOPATH)/bin

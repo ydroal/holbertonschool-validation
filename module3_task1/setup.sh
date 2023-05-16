9f6e835b1243b8f59be5c63cd00e2426067c254b
ydroal <droal.yoko@gmail.com>
2023-05-16 11:24:49 +0200

    add: task1

---
+++ module3_task1/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+
+if ! dpkg -s make >/dev/null 2>&1; then
+  apt-get install -y make
+fi
+if ! dpkg -s curl >/dev/null 2>&1; then
+  apt-get install -y curl
+fi
+if ! dpkg -s hugo >/dev/null 2>&1; then
+  # Download lastest release of 'Hugo'
+  curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+  # Install the latest version of 'Hugo'
+  apt-get install ./install_hugo.deb
+fi
